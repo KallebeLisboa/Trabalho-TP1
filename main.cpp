f434089eb901cae7c0735dee5842ac9e48084b0a
Rian Kallebe da Silva Lisbôa <kallebelisboa@gmail.com>
2025-11-23 18:58:47 -0300

    Final

--- main.cpp
+++ main.cpp
@@ -8,37 +8,36 @@
 #include "header servicos/container_hotel.hpp"
 #include "header servicos/container_quarto.hpp"
 #include "header servicos/container_reserva.hpp"
-#include "header servicos/container_hospede.hpp" // <--- ADICIONADO
-
+#include "header servicos/container_hospede.hpp"
 using namespace std;
 
 int main() {
-    // PASSO 1: Criar os "Bancos de Dados" (Serviços)
+    // Criar os "Bancos de Dados" (Serviços)
     ContainerGerente* servicoGerente = new ContainerGerente();
     ContainerHotel* servicoHotel = new ContainerHotel();
     ContainerQuarto* servicoQuarto = new ContainerQuarto();
     ContainerReserva* servicoReserva = new ContainerReserva();
-    ContainerHospede* servicoHospede = new ContainerHospede(); // <--- ADICIONADO
+    ContainerHospede* servicoHospede = new ContainerHospede();
 
-    // PASSO 2: Criar a Interface (Apresentação)
+    //Criar a Interface (Apresentação)
     ControladoraApresentacao app;
 
-    // PASSO 3: Ligar os fios (Injeção de Dependência)
+    // Injeção de Dependência
     app.configurarServicos(servicoGerente);
     app.configurarServicosHotel(servicoHotel);
     app.configurarServicosQuarto(servicoQuarto);
     app.configurarServicosReserva(servicoReserva);
-    app.configurarServicosHospede(servicoHospede); // <--- ADICIONADO
+    app.configurarServicosHospede(servicoHospede);
 
-    // PASSO 4: Rodar o Sistema
+    // Rodar o Sistema
     app.executar();
 
-    // PASSO 5: Limpeza de memória ao fechar
+    // Limpeza de memória ao fechar
     delete servicoGerente;
     delete servicoHotel;
     delete servicoQuarto;
     delete servicoReserva;
-    delete servicoHospede; // <--- ADICIONADO
+    delete servicoHospede;
 
     return 0;
 }
